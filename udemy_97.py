c407475f55ae3664c74091bf61b01b78ff4cffe5
tora <masaya.development@gmail.com>
2022-01-23 11:07:49 +0900

    CSVファイルへの書き込みと読み込みについて

--- udemy_97.py
+++ udemy_97.py
@@ -4,8 +4,21 @@
 
 import csv
 
+# CSVファイルの書き込み
 with open('test.csv', 'w') as csv_file:
     fieldname = ['Name', 'Count']
     # CSVに書き込む為のオブジェクトを作成する。
     writer = csv.DictWriter(csv_file, fieldnames=fieldname)
+    # カラムの情報を書き込む
     writer.writeheader()
+
+    # カラムに対してデータを書き込む
+    writer.writerow({'Name':'A', 'Count': 1})
+    writer.writerow({'Name': 'B', 'Count': 2})
+
+
+# CSVファイルの読み込み
+with open('test.csv', 'r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['Name'], row['Count'])
